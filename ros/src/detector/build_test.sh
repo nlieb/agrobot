79b087afa4a41648ed585556f3095973658344a3
Nathan Liebrecht <nwliebrecht@gmail.com>
2018-09-15 12:36:36 -0700

    asdf

--- ros/src/detector/build_test.sh
+++ ros/src/detector/build_test.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 export LD_LIBRARY_PATH = "${LD_LIBRARY_PATH}:$(pwd)/darknet"
 g++ -std=c++11  -DOPENCV `pkg-config --cflags opencv` -Wall -Ofast -DOPENCV -fopenmp -fPIC -o go src/yolo.cpp -lm -pthread  `pkg-config --libs opencv`  -lgomp -L ./ -l:darknet/darknet.so
