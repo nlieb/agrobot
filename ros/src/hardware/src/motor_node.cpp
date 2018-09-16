28ac07950b59d8fd52fead1f0017d7ec4bcdf540
Nathan Liebrecht <nwliebrecht@gmail.com>
2018-09-16 02:33:38 +0000

    basic motor stuff

--- ros/src/hardware/src/motor_node.cpp
+++ ros/src/hardware/src/motor_node.cpp
@@ -1,24 +1,47 @@
 
+#include <ros/ros.h>
+#include <jhpwmpca9685.h>
+#include <std_msgs/Bool.h>
+
+
+PCA9685 *pca9685 = new PCA9685();
+
+void motorStartCb(const std_msgs::Bool &msg) {
+  printf("base speed cb\n");
+
+  if(msg.data) {
+    pca9685->setPulseLength(0, 1000);
+    pca9685->setPulseLength(1, 1000);
+  } else {
+    pca9685->setPulseLength(0, 0);
+    pca9685->setPulseLength(1, 0);
+  }
+}
+
 int main(int argc, char** argv) {
   ros::init(argc, argv, "motor_node");
   ros::NodeHandle nh;
   ros::NodeHandle private_nh;
 
-  // read params
-  int start_btn_gpio = 481;
-  int rate = 2;
+  int err = pca9685->openPCA9685();
+  if (err < 0) {
+    ROS_ERROR("Error: %d", pca9685->error);
+  }
 
-  ros::Publisher start_pub =
-          nh.advertise<std_msgs::Bool>("/gpio/write", 4);
+  ROS_DEBUG("PCA9685 Device Address: 0x%02X\n", pca9685->kI2CAddress);
+  pca9685->setAllPWM(0, 0);
+  pca9685->reset();
+  pca9685->setPWMFrequency(200);
+  pca9685->setAllPWM(0, 0);
 
-  ros::Rate r(rate);
+  ros::Subscriber start_sub =
+    nh.subscribe("motor/start", 1, &motorStartCb);
+
+  ros::Rate r(30);
   std_msgs::Bool start_button;
 
   while (ros::ok()) {
-    gpioGetValue(start_btn_gpio, &mission_start);
-
-    start_button.data = static_cast<unsigned char>(not mission_start);
-    start_pub.publish(start_button);
+//    start_pub.publish(start_button);
 
     ros::spinOnce();
     r.sleep();
